ea937722f27575ee20e20a082c2e2f075f1ae160
Clark <osborneclark5@gmail.com>
2026-03-12 11:21:46 -0400

    Did the Week 10 Homework with courtines!

---
+++ Assets/Scripts/Week9-14/Grower.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class Grower : MonoBehaviour
+{
+    public Transform treeTransform;
+    public Transform appleTransform;
+    public float appleDelay = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        StartCoroutine(GrowTree());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartTreeGrowing()
+    {
+        StartCoroutine(GrowTree());
+    }
+
+    IEnumerator GrowTree()
+    {
+        float t = 0;
+        treeTransform.localScale = Vector2.zero;
+        appleTransform.localScale = Vector2.zero;
+
+        while(t < 1)
+        {
+            t += Time.deltaTime;
+            treeTransform.localScale = Vector2.one * t;
+            yield return null;
+            //yield return new WaitForSeconds(appleDelay);
+        }
+
+        yield return new WaitForSeconds(appleDelay);
+    
+        t = 0;
+
+        while(t < 1)
+        {
+            t += Time.deltaTime;
+            appleTransform.localScale = Vector2.one * t;
+            yield return null;
+        }
+    }
+}
