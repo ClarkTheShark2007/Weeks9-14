6344450d9099bf264c82ad5d54554bbad4db7dac
osborncl <osborncl@OA-S141-08>
2026-04-07 11:31:42 -0400

    Did the coding the Gym!

--- Assets/Scripts/Week9-14/LocalMultiplayerController.cs
+++ Assets/Scripts/Week9-14/LocalMultiplayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,8 +8,18 @@ public class LocalMultiplayerController : MonoBehaviour
     public PlayerInput playerInput;
     public Vector2 movementInput;
     public float speed = 5f;
+    public TrailRenderer trailRenderer;
+    public ParticleSystem ps;
+    //public TrailRenderer
 
+    Coroutine theDashingCoroutine;
     // Update is called once per frame
+    private void Start()
+    {
+        Sprite spriteRenderer = GetComponent<Sprite>();
+        ps.textureSheetAnimation.SetSprite(0, spriteRenderer);
+    }
+
     void Update()
     {
         transform.position += (Vector3) movementInput * speed * Time.deltaTime;
@@ -26,6 +37,28 @@ public class LocalMultiplayerController : MonoBehaviour
             Debug.Log("Player " + playerInput.playerIndex + ": Attack!");
             manager.PlayerAttacking(playerInput);
         }
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if(context.performed == true)
+        {
+            if(theDashingCoroutine != null) 
+            {
+                StopCoroutine(Dash());
+            }
+
+            theDashingCoroutine = StartCoroutine(Dash());
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        trailRenderer.emitting = true;
+        speed = 10f;
+        yield return new WaitForSeconds(1f);
+        trailRenderer.emitting = false;
+        speed = 5f;
     }
 
 }
--- Assets/Scripts/Week9-14/LocalMultiplayerManager.cs
+++ Assets/Scripts/Week9-14/LocalMultiplayerManager.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.ParticleSystem;
 
 public class LocalMultiplayerManager : MonoBehaviour
 {
     public List<Sprite> playerSprites;
     public List<PlayerInput> players;
+
+    public CinemachineImpulseSource impulseSource;
 
     public void OnPlayerJoined(PlayerInput player)
     {
@@ -27,6 +31,10 @@ public class LocalMultiplayerManager : MonoBehaviour
             if (Vector2.Distance(attackingPlayer.transform.position, players[i].transform.position) < 0.5f)
             {
                 Debug.Log("Player " + attackingPlayer.playerIndex + " hit player " + players[i].playerIndex);
+                impulseSource.GenerateImpulse();
+
+                ParticleSystem bloodparticles = players[i].GetComponent<ParticleSystem>();
+                bloodparticles.Emit(10);
             }
         }
     }
